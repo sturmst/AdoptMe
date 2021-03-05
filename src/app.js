d264bd1a19c711e9bce92e138b4c2fb7e118710a
ststurm <steffi.b.sturm@gmail.com>
2021-03-05 20:45:00 +0100

    added script code

---
+++ src/app.js
@@ -0,0 +1,42 @@
+// throw new Error("ai, ca fait mal");
+
+const Pet = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Noam"),
+        React.createElement("h2", {}, "Domesticated Human"),
+        React.createElement("h2", {}, "From the east"),
+    ]);
+}
+
+const App = () => {
+    // https://frontendmasters.com/courses/complete-react-v5/createelement-arguments/
+    // 1. what kind of element is it? (h1) 
+    // 2. empty object ({}); all attribute you want to give your component, what you want to pass to your child element
+    //  3. children; what ever children you want to pass in this element; variable verity
+    return React.createElement(
+        "div",
+        {id: "pet-container"},
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet, 
+                {
+                    name: "Noam",
+                    animal: "Domesticated Human",
+                    origin:"From the east"
+                }),
+            React.createElement(Pet, 
+                {
+                    name: "Brutus",
+                    animal: "Domesticated Cat",
+                    origin:"Brussels"
+                }),
+            React.createElement(Pet, 
+                {
+                    name: "Sirius",
+                    animal: "Domesticated Cat",
+                    origin: "Sint-Gilles"
+                })
+        ]
+    );
+};
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
